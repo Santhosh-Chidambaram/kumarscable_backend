8674a7cfa8ac0d5ac555616e0ecf96590381bb00
Santhosh-Chidambaram <santhoshsmart111@gmail.com>
2020-05-20 19:24:25 +0530

    gooddd

--- kumarscable_backend/settings.py
+++ kumarscable_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'soin)1c6l&9vs!ep(*s2=qrq*4pc()392ij$su5ok-gjp1vt20$h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1','kumarscable.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1','testkcapp.herokuapp.com']
 
 # Application definition
 
